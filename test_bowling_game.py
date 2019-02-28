769d3d170ff391a2aacaf72fe01eee9fcdc38f74
Dharmesh Tarapore <dharmesh@bu.edu>
2019-02-28 16:30:46 -0500

    Call tests

--- test_bowling_game.py
+++ test_bowling_game.py
@@ -2,7 +2,12 @@ import unittest
 
 from game import Game
 
+
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
+    
+
+if __name__ == "__main__":
+    unittest.main()
